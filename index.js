2287b552f0943be92003a536e1a22f578838770d
barbara huesca perez <barbyhuesca@hotmail.com>
2022-08-23 18:50:48 -0300

    primer comentario

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+
+for (let i = 10; i <= 19; i++) {
+    //*en cada repeticion solicitamos un nombre para asignarle un horario para atenderse con el odontologo Jorge*//
+    let ingresarnombre = prompt ("Porfavor ingrese su nombre");
+    //*confirmamos el horario asignado para cada persona cronologicamente*//
+    alert ("Gracias"+ " " + ingresarnombre + ", " + "Su turno es a las" + " " + i + " "+ "hrs. Porfavor agende con usted esta informacion");
+}
