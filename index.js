ab74ad0160d8056f80609a35a2d966f482c4a63b
barbara huesca perez <barbyhuesca@hotmail.com>
2022-08-27 19:52:06 -0300

    agregando funciones y ciclos

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 
+/* simulacion de compra en una pagina de maquillaje. le solicitamo al cliente que ingrese el tipo y cantidad de productos 
+que desea adquirir   */
 
-
-for (let i = 10; i <= 19; i++) {
-    //*en cada repeticion solicitamos un nombre para asignarle un horario para atenderse con el odontologo Jorge*//
-    let ingresarnombre = prompt ("Porfavor ingrese su nombre");
-    //*confirmamos el horario asignado para cada persona cronologicamente*//
-    alert ("Gracias"+ " " + ingresarnombre + ", " + "Su turno es a las" + " " + i + " "+ "hrs. Porfavor agende con usted esta informacion");
+function nombreProducto (){
+    let nombreIngresado = prompt (" Ingresar nombre del producto que quiere adquirir");
+    alert ("Usted a solicitado el producto" +" "+ nombreIngresado)
+    let cantidades = prompt ("Porfavor indique la cantidad de unidades que desea adquirir");
+    alert("Usted a solicitado" + " " + cantidades + " " + "unidades")
+    let mensaje = ("Sumamos a su carrito" + " " + cantidades + " " + "unidad/es del prodcuto"+ " "+ nombreIngresado );
+    console.log (mensaje)
 }
+
+nombreProducto();
+nombreProducto();
+nombreProducto();
+console.log ("Usted esta llevando 3 tipos de productos")
+
+let entrada2 = prompt ("Desea agregar mas productos a su carrito? Indique SI o NO");
+if ((entrada2 == "si") || (entrada2 == "SI")){
+    alert ("Aguarde unos segundos, se esta abriendo nuevamente su carrito");
+
+} else if ((entrada2 == "no") || (entrada2 == "NO")){
+    alert ("Aguarde unos segundos, se esta abriendo la pestaña para ir a abonar")
+} else {
+    alert ("Usted no ha ingresado un dato valido")
+}
+
+ 
+
+
