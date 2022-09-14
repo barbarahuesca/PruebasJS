862d01a21848979a8108c8d275d4666c787a8cd2
barbara huesca perez <barbyhuesca@hotmail.com>
2022-09-13 22:28:56 -0300

    interactuando con el html

--- index.js
+++ index.js
@@ -1,83 +1,39 @@
+class ClaseDeMaquillaje {
+   constructor(titulo, imagen, duracion) {
+       this.titulo = titulo;
+       this.imagen = imagen;
+       this.duracion = duracion;
+   }
+}
 
-/*Simulacion de Agenda para maquilladora que debe revisar,registrar y/o eliminar turnos*/
+const ClasesDeMaquillaje = [
+   new ClaseDeMaquillaje("Clases de Maquillaje Social", "./imagenes/galeria2.jpeg ", "5 Meses"),
+   new ClaseDeMaquillaje("Clases de Maquillaje Artistico", "./imagenes/galeria9.jpeg", "7 Meses"),
+];
 
+function MostrarClases(ClasesDeMaquillaje) {
+   const contenedorDeClases = document.getElementById("contenedor-de-clases");
+   contenedorDeClases.innerHTML = "";
 
+   ClasesDeMaquillaje.forEach((ClaseDeMaquillaje) => {
+       const divClaseDeMaquillaje = document.createElement("div");
+       divClaseDeMaquillaje.innerHTML = `
+          <img src= "${ClaseDeMaquillaje.imagen}" alt=${ClaseDeMaquillaje.titulo};>
+          <h3>${ClaseDeMaquillaje.titulo}</h3>
+          <p> Duracion: ${ClaseDeMaquillaje.duracion} </p>
+       `;
 
-let option;
- let tasks = [
-   {
-      id: 1,
-      day: "12/09", 
-      costumer : "Maria Eugenia Gonzalez",
-      time: "19 Hrs",
-      service : "Social"
-   },
-   {
-      id:2,
-      day:"15/08",
-      costumer: "Sofia Perez",
-      time: "14 hrs",
-      service: "Artistico"
-   },
-   {
-      id:3,
-      day: "17/10",
-      costumer: "Norma Novillo",
-      time: "15 hrs",
-      service: "Artistico"
-   },
- ];
+       // Primero debe agregarse el contenedor al html antes de poder agregarle clases
+       contenedorDeClases.appendChild(divClaseDeMaquillaje);
 
- while(option !== 0) {
-      option = Number(prompt("Ingrese una opcion:\n1. Agregar Turno\n2. Ver Turnos\n3. Cancelar Turno\n0. Salir "));
+       divClaseDeMaquillaje.classList.add("claseDeMaquillaje");
 
-      switch (option){
-        case 1:
-                const costumer = prompt("Ingrese el nombre del cliente 👩");
-                const day = prompt(" Ingrese el dia en el que quiere agendar la clase📆: registrar en formato XX/XX");
-                const service = prompt("Tipo de maquillaje a realizar: Artistico o Social 💄");
-                const time = String(prompt("Indique horario y agregue AM o PM segun corresponda ⌚"));
-               const creationId = getLastId() + 1;
-               createTask (creationId,costumer, day, service, time,);
-         break;
-       case 2:
-             getAllTasks(); 
-            break;
-       case 3:
-            let deleteId = Number(prompt("Ingrese el ID del turno que desea eliminar: "))   ;
-              deleteTask(deleteId);
-            break;
-        case 0: 
-             alert("Gracias, Vuelva pronto 😊");
-            break;
-        default:
-             alert("Dato invalido, por favor intentelo nuevamente 😬");
-             break;              
-      }
- }
+       //  divClaseDeMaquillaje.classlist.add("ClaseDeMaquillaje");
 
- function createTask( id, costumer, day, service, time){
-    tasks.push({
-        id,
-        costumer,
-        day,
-        time,
-        service,
-    });
- }
-
- function getAllTasks(){
-    console.log("\n------------------------------------------\n");
-    tasks.forEach((task) => console.log(task.id + " - " + task.costumer + " - "+ task.day + " - "+ "Clases de Maquillaje" +" "+  task.service + " - " + task.time ));
- }
-
- function deleteTask (id){
-    tasks = tasks.filter (task => task.id != id);
- }
-
- function getLastId() {
-    const ids = tasks.map(task => task.id);
-    return Math.max(...ids);
- }
-
-
+       //----------
+       //   const botonVerDetalle = crearBotonVerDetalle(ClaseDeMaquillaje);
+       //   divClaseDeMaquillaje.appendChild(botonVerDetalle);
+       //----------
+   });
+}
+MostrarClases(ClasesDeMaquillaje);
