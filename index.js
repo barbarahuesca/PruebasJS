1d79d7728fa43296b32d1e2e471191eb383e767c
barbara huesca perez <barbyhuesca@hotmail.com>
2022-09-26 14:19:56 -0300

    JASON + Storage

--- index.js
+++ index.js
@@ -1,90 +1,117 @@
-class ClaseDeMaquillaje {
-   constructor(titulo, identificador, imagen, duracion, descripcion,precio, ) {
-       this.titulo = titulo;
-       this.identificador = identificador;
-       this.imagen = imagen;
-       this.duracion = duracion;
-       this.descripcion = descripcion;
-       this.precio = precio;
-      
+//* Agenda personalizada para cada clienta  *//
+
+
+class Turno {
+   constructor(nombre, numero, tipo, dia,hora) {
+     this.nombre = nombre;
+     this.numero = numero;
+     this.tipo = tipo;
+     this.dia = dia;
+     this.hora = hora;
    }
 }
 
-const clasesDeMaquillaje = [
-   new ClaseDeMaquillaje(
-       "Clases de Maquillaje Social",
-       "maquillaje-social",
-       "./imagenes/galeria2.jpeg",
-       "5 Meses",
-       "Este curso consta de cuatro meses, en el que aprenderás un monton de tecnicas y tips para aprender a maquillarte en tu dia a dia, asi como tambien para algun evento o cumpleaños. Con una clase semanal de 2 horas, se vera el preparado de piel necesario, asi como tambien que tipos de productos son recomentables a corde al tipo de rostro y piel con el que cuenta cada una. Dependiendo varios factores, se definira los tipos de colores que resaltan para vos misma, estilos y tecnicas adaptados 100% a vos!",
-       "$30.000",
-   ),
-   new ClaseDeMaquillaje(
-       "Clases de Maquillaje Artistico",
-       "maquillaje-artistico",
-       "./imagenes/galeria9.jpeg",
-       "7 Meses",
-       "En este curso de cuatro meses, aprenderas a como realizarte maquillajes artisticos, destinados usualmente para shows, espectaculos,muestras,sesiones de fotos y demas. La idea es que aprendas muchas tecnicas y combinacion de colores. En este curso se apela mucho a la creatividad y la imaginacion. Vamos a crear y jugar mucho con colores, delineados, pestañas, labiales y demas. IMPORTANTE: se espera tener conocimientos minimos de maquillaje para un mejor aprendizaje",
-       "$50.000",
+let nombreUsuario;
 
-   ),
-];
-
-function mostrarClases(arrayClases) {
-   const contenedorDeClases = document.getElementById("contenedor-de-clases");
-   contenedorDeClases.innerHTML = "";
+document.getElementById("formulario-usuario").addEventListener("submit", manejadorFormularioUsuario);
  
-   arrayClases.forEach((clase) => {
-     const divClaseDeMaquillaje = document.createElement("div");
-     divClaseDeMaquillaje.innerHTML = `
-       <div class="contenido">
-            <img src= "${clase.imagen}" alt=${clase.titulo};>
-             <h3>${clase.titulo}</h3>
-            <p> Duracion: ${clase.duracion} </p>
-            <button id="mostrarDetalle${clase.identificador}" class=boton-ver-detalle"> Ver Detalle</button>
-         </div>
-       `;
-     contenedorDeClases.appendChild(divClaseDeMaquillaje);
+function manejadorFormularioUsuario(e) {
+   console.log(e);
+   e.preventDefault();
+   nombreUsuario = document.getElementById("user").value;
  
-
+   const listadodeTurnos= document.getElementById("listadodeTurnos");
+   const turnos = JSON.parse(localStorage.getItem(nombreUsuario));
+ 
+   if (turnos == null) {
+      listadodeTurnos.innerHTML = "<h1>No hay turnos para mostrar</h1>"
+    } else {
+      mostrarTurnos(turnos);
+    }
+    mostrarPanel();
+}
+ 
+function mostrarTurnos(turnos) {
+   let listadodeTurnos = document.getElementById("listadodeTurnos");
+   listadodeTurnos.innerHTML = "";
+ 
+   turnos.forEach(turno => {
+     let li = document.createElement("li");
+     li.innerHTML = `
+     <hr> ${turno.nombre.toUpperCase()} - ${turno.numero} - 
+     ${turno.tipo} - ${turno.dia} - ${turno.hora}
+     `;
+     const botonBorrar = crearBotonEliminar(turno);
+     li.appendChild(botonBorrar);
+     listadodeTurnos.appendChild(li);
    });
 }
-
-function mostrarClases(arrayClases) {
-   const contenedorDeClases = document.getElementById("contenedor-de-clases");
-   contenedorDeClases.innerHTML = "";
-   arrayClases.forEach((clase) => {
-     const divClaseDeMaquillaje = document.createElement("div");
-     //ACA ABAJO LE AGREGO EL DIV CON LA DESCRIPCION ESTE VA ESTAR OCULTO POR LA CLASE VISIBLE QUE TIENE UN DISPLAY NONE, TMB LE DOI UN NOMBRE AL DIV CON EL IDENTIFICADOR PARA JUSTAMENTE PODER IDENTIFICARLO 
-     divClaseDeMaquillaje.innerHTML = `
-       <div class="contenido-${clase.identificador}">
-            <img src= "${clase.imagen}" alt=${clase.titulo};>
-             <h3>${clase.titulo}</h3>
-             <div class="div-${clase.identificador} visible">
-             <p> Descripcion: ${clase.descripcion} </p>
-             </div>
-            <p> Duracion: ${clase.duracion} </p>
-            <button id="mostrarDetalle${clase.identificador}" class=boton-ver-detalle"> Ver Detalle/ Ver Menos</button>
-         </div>
-       `;
-     contenedorDeClases.appendChild(divClaseDeMaquillaje);
-   });
- }
-
- mostrarClases(clasesDeMaquillaje);
-
- document.addEventListener("click", (e) => {
-   if (e.target.matches("#mostrarDetallemaquillaje-social")) {
-     //ACA SELECCIONO EL DIV CON EL NOMBRE DEL IDENTIFICADOR Q YA LE HABIA DADO
-     let div1 = document.querySelector(".div-maquillaje-social");
-     //ACA CON TOGGLE LO Q HACE ES QUE CADA VEZ Q LE DEN CLICK  LE SACA O LE PONE LA CLASE VISIBLE
-     div1.classList.toggle("visible");
-   }
  
-   if (e.target.matches("#mostrarDetallemaquillaje-artistico")) {
-     //ACA LOS MISMO PERO CON LA DE MAQUILLAJE ARTISTICO
-     let div2 = document.querySelector(".div-maquillaje-artistico");
-     div2.classList.toggle("visible");
-   }
- });
+function crearBotonEliminar(turno){
+   const botonBorrar = document.createElement("button");// <button>Borrar</button>
+     botonBorrar.innerText = "Borrar";
+     botonBorrar.addEventListener("click", () => {
+       eliminarTurno(turno);
+     })
+     return botonBorrar;
+}
+ 
+function mostrarPanel() {
+   const opciones = document.getElementById("opciones");
+ 
+   opciones.innerHTML =
+     `<h3>Agenda de turnos para ${nombreUsuario} <3 </h3>
+     <form id="formulario-turno">
+       <input type="text" id="nombre" placeholder="Apellido">
+       <input type="number" id="numero" placeholder="Numero de Clase">
+       <input type="text" id="tipo" placeholder="Tipo de Maquillaje">
+       <input type="text" id="dia" placeholder="Dia">
+       <input type="text" id="hora" placeholder="Hora">
+       <button type="submit">Agregar turno</button>
+     </form>`;
+ 
+   document.getElementById("formulario-turno").addEventListener("submit", agregarTurno);
+}
 
+ 
+function validarCampos(turno) {
+   if(turno.nombre == ""){
+     alert("El nombre no puede ser vacio")
+   }
+}
+ 
+function agregarTurno(e) {
+   e.preventDefault();
+   const nombre = document.getElementById("nombre").value;
+   const numero = document.getElementById("numero").value;
+   const tipo = document.getElementById("tipo").value;
+   const dia = document.getElementById("dia").value;
+   const hora = document.getElementById("hora").value;
+ 
+   const turno = new Turno (nombre, numero, tipo, dia,hora);
+ 
+   validarCampos(turno)
+ 
+   const turnosEnLocalStorage = JSON.parse(localStorage.getItem(nombreUsuario));
+ 
+   // 1) me traigo las cosas que necesito del locaslStorage
+   // 2) Agregago mi cosa
+   // 3) vuelvo a subir al localStorage.
+ 
+   if (turnosEnLocalStorage == null) {
+     localStorage.setItem(nombreUsuario, JSON.stringify([turno]));
+     mostrarTurnos([turno]);
+   } else {
+     turnosEnLocalStorage.push(turno);
+     localStorage.setItem(nombreUsuario, JSON.stringify(turnosEnLocalStorage));
+     mostrarTurnos(turnosEnLocalStorage);
+   }
+   e.target.reset();
+}
+ 
+function eliminarTurno(turno) {
+   const turnosEnLocalStorage = JSON.parse(localStorage.getItem(nombreUsuario));
+   const nuevoArray = turnosEnLocalStorage.filter(item => item.nombre != turno.nombre);
+   localStorage.setItem(nombreUsuario, JSON.stringify(nuevoArray));
+   mostrarTurnos(nuevoArray);
+}
